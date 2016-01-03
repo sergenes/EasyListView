62fae488cfc83eadf5c1055edf4d95a9977f516a
Serge Neskoromny <serge.nes@gmail.com>
2016-01-03 22:40:55 +0200

    + Refactoring; + Added abstract classes BaseEasyRow and BaseEasyHeader;

--- easylistview/src/main/java/org/nes/easylistview/EasyListView.java
+++ easylistview/src/main/java/org/nes/easylistview/EasyListView.java
@@ -2,6 +2,7 @@ package org.nes.easylistview;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -14,7 +15,7 @@ import java.util.ArrayList;
  */
 public class EasyListView extends ListView {
 
-    ArrayList<RowHolder> rowSource = new ArrayList();
+    ArrayList<RowHolder> rowSource = new ArrayList<RowHolder>();
 
     public ArrayList<RowHolder> getRowSource() {
         return rowSource;
@@ -80,5 +81,65 @@ public class EasyListView extends ListView {
         void update();
 
         void onClick();
+
+        int getTag();
+    }
+
+    public static abstract class BaseEasyRow implements RowHolder {
+        LayoutInflater layoutInflater;
+
+        int layout;
+
+        int tag = 0;
+
+        public BaseEasyRow(Context context, int layout) {
+            this.layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            this.layout = layout;
+        }
+
+        public View getInflatedView(ViewGroup parent) {
+            View view = layoutInflater.inflate(layout, parent, false);
+            view.setTag(tag);
+            return view;
+        }
+
+        public void setTag(int tag) {
+            this.tag = tag;
+        }
+
+        public int getTag() {
+            return tag;
+        }
+
+
+        @Override
+        public void setDefaultValues() {
+
+        }
+
+        @Override
+        public void update() {
+
+        }
+
+        @Override
+        public void onClick() {
+
+        }
+
+    }
+
+    public static abstract class BaseEasyHeader extends BaseEasyRow {
+
+        public BaseEasyHeader(Context context, int layout) {
+            super(context, layout);
+        }
+
+        public View getInflatedView(ViewGroup parent) {
+            View header = layoutInflater.inflate(layout, parent, false);
+            //prevent onItemClick
+            header.setClickable(true);
+            return header;
+        }
     }
 }
